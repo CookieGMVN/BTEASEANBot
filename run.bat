7de2ace583b15bc965ee4e8437285e9cfca4ae4c
CookieGMVN <73780382+CookieGMVN@users.noreply.github.com>
2021-02-17 19:00:32 +0700

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+title BTE ASEAN || Southeast Asia
+echo Ckecking for updates...
+call npm i
+echo.
+echo Starting...
+echo.
+echo When it loaded, please press Ctrl-C to turn off this bot.
+call node index.js
